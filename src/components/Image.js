add4dbafed8928805d195edc8f9f695707ca0f53
Jason Jaramillo <jason.araque@hotmail.com>
2022-01-28 07:06:35 +0000

    Add hear and cart icons to images

--- src/components/Image.js
+++ src/components/Image.js
@@ -3,12 +3,17 @@ import React, { useState } from "react"
 function Image({ className, img }) {
     const [hovered, setHovered] = useState(false)
 
-    console.log(hovered)
+    const heartIcon = hovered && <i className="ri-heart-line favorite"></i>
+    const cartIcon = hovered && <i className="ri-add-circle-line cart"></i>
     return (
         <div
             className={`${className} image-container`}
             onMouseEnter={() => setHovered(true)}
-            onMouseLeave={() => setHovered(false)}>
+            onMouseLeave={() => setHovered(false)}
+        >
+            {heartIcon}
+            {cartIcon}
+
             <img src={img} className="image-grid" alt="" />
         </div>
     )
